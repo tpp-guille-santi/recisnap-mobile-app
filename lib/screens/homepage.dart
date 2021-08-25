b575d9c67fb08e514016a8038510aad945a6715e
Guillermo Czop <guilleczop@gmail.com>
2021-08-25 20:38:50 -0300

    Menu principal - Ajustados tamaños
    * Se ajusta el tamaño de los componentes para que quede en el centro.
    * Se agrega padding a la columna principal para controlar el tamaño horizontal

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -8,26 +8,29 @@ class Homepage extends StatelessWidget {
     return Scaffold(
       backgroundColor: LIGHT_GREEN_COLOR,
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            Image.network(
-                'https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/8/88/G.svg/revision/latest/scale-to-width-down/200?cb=20160125094907'),
-            HomepageButton(
-              onPressed: null,
-              buttonText: 'Sacar foto',
-            ),
-            HomepageButton(
-              onPressed: null,
-              buttonText: 'Ver materiales',
-            ),
-            HomepageButton(
-              onPressed: null,
-              buttonText: '¿Por que reciclar?',
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 60.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Image.network(
+                  'https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/8/88/G.svg/revision/latest/scale-to-width-down/200?cb=20160125094907'),
+              HomepageButton(
+                onPressed: null,
+                buttonText: 'Sacar foto',
+              ),
+              HomepageButton(
+                onPressed: null,
+                buttonText: 'Ver materiales',
+              ),
+              HomepageButton(
+                onPressed: null,
+                buttonText: '¿Por que reciclar?',
+              ),
+            ],
+          ),
         ),
       ),
     );
