6bdc9decd74159d28141c44eef1cca282d47379c
Guillermo Czop <guilleczop@gmail.com>
2022-02-21 11:08:08 -0300

    Cambios

--- lib/screens/materialsCatalogueScreen.dart
+++ lib/screens/materialsCatalogueScreen.dart
@@ -26,17 +26,43 @@ class MaterialsCatalogue extends StatelessWidget {
               child: ListView(
                 children: [
                   MaterialCard(
-                    title: 'title',
-                    body: 'body',
-                  ),
+                      title: 'Plástico',
+                      body: 'body',
+                      image: Image.network(
+                          'https://cdn-icons-png.flaticon.com/512/1996/1996869.png')),
                   MaterialCard(
-                    title: 'title2',
+                    title: 'Vidrio',
                     body: 'body2',
+                    image: Image.network(
+                        'https://cdn-icons-png.flaticon.com/512/85/85843.png'),
                   ),
                   MaterialCard(
-                    title: 'title3',
+                    title: 'Cartón',
                     body: 'body3',
-                  )
+                    image: Image.network(
+                        'https://cdn-icons-png.flaticon.com/512/65/65686.png'),
+                  ),
+                  MaterialCard(
+                      title: 'Pilas',
+                      body: 'body3',
+                      image: Image.network(
+                        'https://static.thenounproject.com/png/187466-200.png',
+                      )),
+                  MaterialCard(
+                      title: 'Papel',
+                      body: 'body3',
+                      image: Image.network(
+                          'https://thumbs.dreamstime.com/b/document-icon-vector-illustration-design-flat-paper-icon-vector-document-icon-vector-illustration-design-flat-paper-icon-vector-164319261.jpg')),
+                  MaterialCard(
+                      title: 'Metal',
+                      body: 'body3',
+                      image: Image.network(
+                          'https://www.pikpng.com/pngl/b/287-2878853_png-icon-the-metal-icon-clipart.png')),
+                  MaterialCard(
+                      title: 'Basura',
+                      body: 'body3',
+                      image: Image.network(
+                          'https://png.pngtree.com/png-vector/20190326/ourlarge/pngtree-vector-trash-icon-png-image_865253.jpg'))
                 ],
               ),
             ),
--- lib/widgets/materialCard.dart
+++ lib/widgets/materialCard.dart
@@ -4,8 +4,9 @@ import 'package:recyclingapp/consts.dart';
 class MaterialCard extends StatelessWidget {
   String title;
   String body;
+  Image image;
 
-  MaterialCard({required this.title, required this.body});
+  MaterialCard({required this.title, required this.body, required this.image});
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +15,44 @@ class MaterialCard extends StatelessWidget {
         splashColor: Colors.greenAccent,
         onTap: () {
           print('Card tapped.');
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Material'),
+              content: Scrollbar(
+                child: SingleChildScrollView(
+                  child: const Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas vestibulum purus nec facilisis. Mauris interdum libero ut massa venenatis, nec dapibus sem tempor. In aliquam faucibus lectus in tincidunt. Ut vehicula eros lacus, vitae sodales tortor pharetra ac. Nam risus purus, ornare non dolor ac, porta mattis urna. Donec ullamcorper consectetur velit a efficitur. Nulla vel efficitur ligula. Vivamus vel consequat diam. Nam faucibus turpis felis, eget elementum quam lacinia ac. Aliquam ultrices ipsum tincidunt facilisis maximus. Ut rutrum ut sem at efficitur. Proin ullamcorper tristique neque at maximus. Duis convallis dapibus sapien, a euismod erat volutpat et. Aenean quis rutrum est, et tempor neque. Suspendisse imperdiet suscipit varius.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas vestibulum purus nec facilisis. Mauris interdum libero ut massa venenatis, nec dapibus sem tempor. In aliquam faucibus lectus in tincidunt. Ut vehicula eros lacus, vitae sodales tortor pharetra ac. Nam risus purus, ornare non dolor ac, porta mattis urna. Donec ullamcorper consectetur velit a efficitur. Nulla vel efficitur ligula. Vivamus vel consequat diam. Nam faucibus turpis felis, eget elementum quam lacinia ac. Aliquam ultrices ipsum tincidunt facilisis maximus. Ut rutrum ut sem at efficitur. Proin ullamcorper tristique neque at maximus. Duis convallis dapibus sapien, a euismod erat volutpat et. Aenean quis rutrum est, et tempor neque. Suspendisse imperdiet suscipit varius.',
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'OK'),
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          );
         },
         child: SizedBox(
           width: 300,
-          height: 100,
+          height: 125,
           child: Column(
-            children: [Text(title), Text(body)],
+            children: [
+              Text(title),
+              Row(
+                children: <Widget>[
+                  Container(
+                    child: image,
+                    height: 100.0,
+                    width: 100.0,
+                  ),
+                  Text(body),
+                ],
+              )
+            ],
           ),
         ),
       ),
