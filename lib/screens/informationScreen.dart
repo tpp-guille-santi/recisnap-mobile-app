189c764d196cd3853d2d0e52dcc7f14f7a9caf6a
Guillermo Czop <guilleczop@gmail.com>
2023-06-09 19:54:52 -0300

    InformationScreen wrapped
    -Meto el Container dentro de un scaffold para ocultar el ProgressIndicator del stack inferior

--- lib/screens/informationScreen.dart
+++ lib/screens/informationScreen.dart
@@ -7,23 +7,25 @@ class InformationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        child: FutureBuilder<String>(
-          future: markdownManager.getRecyclingInformation(),
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Markdown(data: 'Please wait its loading...');
-            } else {
-              if (snapshot.hasError)
-                return Markdown(data: 'Error: ${snapshot.error}');
-              else
-                return Markdown(
-                  data: '${snapshot.data}',
-                );
-            }
-          },
-        ),
+    return SafeArea(      
+      child: Scaffold(
+        body: Container(
+          child: FutureBuilder<String>(
+            future: markdownManager.getRecyclingInformation(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Markdown(data: 'Please wait its loading...');
+              } else {
+                if (snapshot.hasError)
+                  return Markdown(data: 'Error: ${snapshot.error}');
+                else
+                  return Markdown(
+                    data: '${snapshot.data}',
+                  );
+              }
+            },
+          ),
+        )
       ),
     );
   }
